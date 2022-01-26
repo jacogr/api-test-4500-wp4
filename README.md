382dda435eb3a8a362a11f939107c3528cf55a03
Jaco <jacogr@gmail.com>
2022-01-26 15:14:53 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+Simple webpack 4 default project.
+
+- `yarn` to install dependencies
+- `yarn build` to compile via webpack
+- open `dist/index.html` in a browser and the current Polkadot blocknumber will display
+
+Differences from WP 5 version -
+
+- plugin to support `import.meta.url`
+- browserslist for Babel in `package.json`
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+  entry: ['regenerator-runtime/runtime', './index.js'],
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+		rules: [
+			{
+				test: /\.js$/,
+				loader: require.resolve('@open-wc/webpack-import-meta-loader'),
+			},
+			{
+				test: /\.js$/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['@babel/env']
+					}
+				}
+			}
+		]
+	}
+};
